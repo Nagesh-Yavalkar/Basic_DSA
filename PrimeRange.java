d80a01a925b7ba12e326688f8387c2f1b9af88a7
Nagesh-Yavalkar <nageshyavalkar@gmail.com>
2026-09-15 00:36:41 +0530

    Initial commit

---
+++ PrimeRange.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PrimeRange{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your low : ");
+        int low = sc.nextInt();
+        System.out.println("Enter your high : ");
+        int high = sc.nextInt();
+        for(int i=low;i<high;i++){
+            int count = 0;
+            for(int div=2;div*div<=i;div++){
+                if(i%div==0){
+                    count++;
+                    break;
+
+                }
+
+                
+            }
+            if(count==0){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
