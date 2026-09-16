89e121f7e200d246575c123bbe9c58a97db0b0aa
Nagesh-Yavalkar <nageshyavalkar@gmail.com>
2026-09-16 21:08:36 +0530

    Reverse of a Number

---
+++ ReverseNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ReverseNumber
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Number : ");
+        int num = sc.nextInt();
+
+        while(num>0){
+            int rem = num%10;
+            System.out.println(rem);
+            num/=10;
+        }
+    }
+}
